032e4c0853bc1e98b86f5e83ef86c3a17a690b72
domanr <domanr@gmail.com>
2017-08-09 21:11:27 +0200

    Temp sensor has been added

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 import network
+import time
+from machine import Pin
+import onewire, ds18x20
 try:
     import usocket as socket
 except:
     import socket
 
 print('Http webserver startup code')
+pin = Pin(2, Pin.OUT)
+pin.value(1)
+
+led = Pin(14, Pin.OUT)
+led.off()
+
+# the device is on GPIO12
+dat = Pin(12)
+
+# create the onewire object
+ds = ds18x20.DS18X20(onewire.OneWire(dat))
+
+# scan for devices on the bus
+roms = ds.scan()
+print('Found 1-wire devices:', roms)
+
 
 sta_if = network.WLAN(network.STA_IF)
 if not sta_if.isconnected():
@@ -12,7 +31,11 @@ if not sta_if.isconnected():
     sta_if.active(True)
     sta_if.connect('Bubuka', 'gyroshusisalival2016')
     while not sta_if.isconnected():
-        pass
+        pin.value(0)
+        time.sleep(0.1)
+        pin.value(1)
+        time.sleep(0.1)
+    pin.value(1)
 print('network config:', sta_if.ifconfig())
 
 f = open('index.html','r')
@@ -25,6 +48,7 @@ Connection: Closed
 f.close()
 
 def main(micropython_optimize=True):
+    global temp
     s = socket.socket()
 
     # Binding to all interfaces - server will be accessible to other hosts!
@@ -35,6 +59,7 @@ def main(micropython_optimize=True):
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind(addr)
     s.listen(5)
+    pin.value(0)
     print("Listening, connect your browser to http://<this_host>:8080/")
 
     counter = 0
@@ -59,6 +84,13 @@ def main(micropython_optimize=True):
             client_stream = client_sock
 
         print("Request:")
+        pin.value(1)
+        time.sleep(0.2)
+        pin.value(0)
+        time.sleep(0.1)
+        pin.value(1)
+        time.sleep(0.2)
+        pin.value(0)
         req = client_stream.readline()
         print(req)
         while True:
@@ -66,7 +98,18 @@ def main(micropython_optimize=True):
             if h == b"" or h == b"\r\n":
                 break
             print(h)
-        client_stream.write(CONTENT)
+        ledOn = req.decode('utf-8').find('ledON')
+        ledOff = req.decode('utf-8').find('ledOFF')
+        if ledOn > 0:
+            led.on()
+        if ledOff > 0:
+            led.off()
+        ds.convert_temp()
+        time.sleep_ms(750)
+        temp = 0
+        for rom in roms:
+            temp = ds.read_temp(rom)
+        client_stream.write(CONTENT % temp)
 
         client_stream.close()
         if not micropython_optimize:
